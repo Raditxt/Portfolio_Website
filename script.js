6a29b2b5ed31b9b786e8837f940fb6f1a4a1629a
Raditya Mulya Akbar <radityamulyaakbar2@gmail.com>
2024-06-04 16:15:14 +0800

    Make some change ind About section, Pendidikan, and footer

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
+  strings : ["Student","Youtuber","Developer"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
